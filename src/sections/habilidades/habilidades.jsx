ef2c9bea312072dec4cdacdf3197a9a193b503a2
rmuratah <rahmurata12@gmail.com>
2024-02-05 18:04:11 -0300

    att

--- src/sections/habilidades/habilidades.jsx
+++ src/sections/habilidades/habilidades.jsx
@@ -3,23 +3,33 @@ import styled from "styled-components"
 
 const Habilidades = () => {
 
-    return (
-        <Section color={'#141414'}>
-            <HabilidadesBox>
-                <TituloBox>
-                    <H1>Habilidades</H1>
-                </TituloBox>
-            </HabilidadesBox>
-        </Section>
-    )
+	return (
+		<Section color={'#141414'}>
+			<HabilidadesBox>
+				<TituloBox>
+					<H1>Habilidades</H1>
+				</TituloBox>
+				<HabilidadeBox>
+					<H2>Linguagens de programação</H2>
+				</HabilidadeBox>
+				<HabilidadeBox>
+					<H2>Linguagens de programação</H2>
+				</HabilidadeBox>
+				<HabilidadeBox>
+					<H2>Linguagens de programação</H2>
+				</HabilidadeBox>
+			</HabilidadesBox>
+		</Section>
+	)
 }
 
 export default Habilidades
 
 const HabilidadesBox = styled.div`
     display: grid;
+		align-items: center;
     height: 100%;
-    grid-template-columns: 25% 65%;
+    grid-template-columns: 20% 25% 25% 25%;
 `
 
 const TituloBox = styled.div`
@@ -27,13 +37,35 @@ const TituloBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+		border: 1px solid white;
+		height: 80%;
 `
 
 const H1 = styled.h1`
     color: white;
-    font-size: 3.5rem;
-    letter-spacing: 0.05rem;
+    font-size: 3.25rem;
     writing-mode: vertical-rl;
     text-orientation: upright;
     text-transform: uppercase;
+`
+
+const H2 = styled.h2`
+ 	 	color: white;
+	  font-size: 1.75rem;
+		text-align: center;
+		margin-top: 80px;
+`
+
+const HabilidadeBox = styled.div`
+    display: flex;
+		justify-content: center;
+		border-right: 1px solid white;
+		border-bottom: 1px solid;
+		height: 80%;
+		border-image: 
+    linear-gradient(
+      to bottom, 
+      white, 
+      rgba(0, 0, 0, 0)
+    ) 1 100%;
 `
