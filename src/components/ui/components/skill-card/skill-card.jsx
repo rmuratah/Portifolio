f9a7b43de4e583cd4701af27c9460dd651ece923
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-22 20:15:02 -0300

    terminando de estilizar a seção habilidades

--- src/components/ui/components/skill-card/skill-card.jsx
+++ src/components/ui/components/skill-card/skill-card.jsx
@@ -1,37 +1,38 @@
-import styled from "styled-components"
-import Tilt from "react-vanilla-tilt"
+import styled from "styled-components";
+import Tilt from "react-vanilla-tilt";
 
 const skillCard = ({ Skill, IconImg }) => {
-    return (
-        <Box style={{}} options={{ scale: 2, max: 25 }}>
-            <Icon src={IconImg} />
-            <P>
-                {Skill}
-            </P>
-        </Box>
-    )
-}
+  return (
+    <Box style={{}} options={{ scale: 2, max: 25 }}>
+      <Icon src={IconImg} />
+      <P>{Skill}</P>
+    </Box>
+  );
+};
 
-export default skillCard
+export default skillCard;
 
 const Box = styled(Tilt)`
-    width: 150px;
-    height: 150px;
+  width: 150px;
+  height: 150px;
+  /* margin: 1.5rem; */
+  cursor: pointer;
+  display: flex;
+  color: white;
+  align-items: center;
+  flex-direction: column;
+
+  &:hover {
     border: 1px solid white;
-    /* margin: 1.5rem; */
-    cursor: pointer;
-    display: flex;
-    color: white;
-    align-items: center;
-    flex-direction: column;
-`
+  }
+`;
 
 const Icon = styled.img`
-    width: 75%;
-    height: 75%;
-    margin: 5px;
-`
+  width: 75%;
+  height: 75%;
+  margin: 5px;
+`;
 
 const P = styled.p`
-    margin-top: 5px;
-`
+  margin-top: 5px;
+`;
