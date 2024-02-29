1e342ab4ed85b8c4dd032cb1ff7c468101e3a331
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-28 21:45:24 -0300

    adicionando animação na seção sobre mim

--- src/sections/sobre-mim/sobre-mim.jsx
+++ src/sections/sobre-mim/sobre-mim.jsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Section from "../../components/ui/components/section/section";
-import background from '../../assets/images/background.jpg'
+import background from "../../assets/images/background.jpg";
 
 const sobreMim = () => {
   return (
@@ -17,6 +17,17 @@ const sobreMim = () => {
 };
 
 export default sobreMim;
+
+const fadeDown1 = keyframes`
+  from{
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  to{
+    opacity: 1;
+    transform: translateY(0px);
+  }
+`;
 
 const Box = styled.div`
   display: flex;
@@ -36,15 +47,18 @@ const TextBox = styled.div`
 const H2 = styled.h2`
   color: white;
   font-size: 4.5rem;
+  animation: ${fadeDown1} 1s;
 `;
 
 const H3 = styled.h3`
   color: white;
   font-size: 3.5rem;
+  animation: ${fadeDown1} 1.25s;
 `;
 
 const P = styled.p`
   color: white;
   font-size: 3.5rem;
   margin-top: 2.55rem;
+  animation: ${fadeDown1} 1.5s;
 `;
