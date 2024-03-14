e9b10a143740cbed4848a5d87d7835bb7ffc71cf
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-14 20:46:00 -0300

    mudando a paleta de cores

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,28 @@
+import { styled } from "styled-components";
+
 const Header = () => {
+  return (
+    <Box>
+      <Button>Habilidades</Button>
+      <Button>Redes Sociais</Button>
+      {/* <button></button> */}
+    </Box>
+  );
+};
 
-    return(
-        <header>
-            
-        </header>
-    )
-}
+export default Header;
 
-export default Header
+const Box = styled.header`
+  height: 8vh;
+  display: flex;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  padding: 10px;
+  border: 1px solid black;
+  margin: 20px;
+  cursor: pointer;
+`;
